8384e653bf8b15a193d7b8848e40334dc0271f96
cval-labs <cvjp03@gmail.com>
2023-08-20 18:51:50 -0400

    completed Add and Modify button methods in MainFormController

--- src/main/java/valerio/software1/controllers/MainFormController.java
+++ src/main/java/valerio/software1/controllers/MainFormController.java
@@ -2,12 +2,14 @@ package valerio.software1.controllers;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
-import javafx.scene.control.Label;
-import javafx.scene.control.TableColumn;
-import javafx.scene.control.TableView;
-import javafx.scene.control.TextField;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.*;
+import javafx.stage.Stage;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
@@ -48,40 +50,64 @@ public class MainFormController implements Initializable {
     @FXML
     private TableView<?> mainProductTV;
 
+    Stage stage;
+    Parent scene;
+
+
     @FXML
     void onActionExit(ActionEvent event) {
+        // TODO: add here
         System.out.println("Exit button clicked");
     }
 
     // Parts Methods
     @FXML
-    void onActionAddPart(ActionEvent event) {
+    void onActionAddPart(ActionEvent event) throws IOException {
+        stage = (Stage)((Button)event.getSource()).getScene().getWindow();
+        scene  = FXMLLoader.load(getClass().getResource("/valerio/software1/add-part.fxml"));
+        stage.setScene(new Scene(scene));
+        stage.show();
+
         System.out.println("Add Part button clicked");
     }
 
     @FXML
     void onActionDeletePart(ActionEvent event) {
+        // TODO: add here
         System.out.println("Delete Part button clicked");
     }
 
     @FXML
-    void onActionModifyPart(ActionEvent event) {
+    void onActionModifyPart(ActionEvent event) throws IOException {
+        stage = (Stage)((Button)event.getSource()).getScene().getWindow();
+        scene  = FXMLLoader.load(getClass().getResource("/valerio/software1/modify-part.fxml"));
+        stage.setScene(new Scene(scene));
+        stage.show();
         System.out.println("Modify Part button clicked");
     }
 
     // Product Methods
     @FXML
-    void onActionAddProduct(ActionEvent event) {
+    void onActionAddProduct(ActionEvent event) throws IOException {
+        stage = (Stage)((Button)event.getSource()).getScene().getWindow();
+        scene  = FXMLLoader.load(getClass().getResource("/valerio/software1/add-product.fxml"));
+        stage.setScene(new Scene(scene));
+        stage.show();
         System.out.println("Add Product button clicked");
     }
 
     @FXML
     void onActionDeleteProduct(ActionEvent event) {
+        // TODO: add here
         System.out.println("Delete Product button clicked");
     }
 
     @FXML
-    void onActionModifyProduct(ActionEvent event) {
+    void onActionModifyProduct(ActionEvent event) throws IOException {
+        stage = (Stage)((Button)event.getSource()).getScene().getWindow();
+        scene  = FXMLLoader.load(getClass().getResource("/valerio/software1/modify-product.fxml"));
+        stage.setScene(new Scene(scene));
+        stage.show();
         System.out.println("Modify Product button clicked");
     }
 
